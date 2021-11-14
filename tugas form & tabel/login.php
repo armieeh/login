dcc098ad16bc0487ebb7bb388d2006e1cb3e2f13
armieeh <armisaniasinarya@gmail.com>
2021-11-14 10:56:53 +0700

    first commit

---
+++ tugas form & tabel/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head> 
+       <meta charset="utf-8"> 
+	<title>Membuat Form</title> 
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <form>  
+       <div class="login">
+       	  <h2 class="header"> Login Disini </h2> 
+          
+       	  <div class="input-group">
+       	    <input type="text" required="">
+       	    <span>Username</span> 
+       	  </div>
+
+       	  <div class="input-group">
+       	    <input type="Password" required="">
+       	    <span>Password</span> 
+       	  </div> 
+
+       	  <div class="input-group">
+       	    <input type="submit" value="Login">
+       	  </div>
+       </div> 
+    </form>
+
+</body>
+</html>
---
+++ tugas form & tabel/register.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head> 
+       <meta charset="utf-8"> 
+	<title>Membuat Form</title> 
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <form>  
+       <div class="login">
+       	  <h2 class="header"> Login Disini </h2> 
+          
+       	  <div class="input-group">
+       	    <input type="text" required="">
+       	    <span>Nama</span> 
+       	  </div>
+
+       	  <div class="input-group">
+       	    <input type="Password" required="">
+       	    <span>NIS</span> 
+       	  </div> 
+
+          <div class="input-group">
+            <input type="Password" required="">
+            <span>Alamat</span> 
+          </div> 
+
+          <div class="input-group">
+            <input type="Password" required="">
+            <span>Jenis Kelamin</span> 
+          </div> 
+
+          <div class="input-group">
+            <input type="Password" required="">
+            <span>Password</span> 
+          </div> 
+
+          <div class="input-group">
+            <input type="Password" required="">
+            <span>Konfirmasi Password</span> 
+          </div> 
+          
+       	  <div class="input-group">
+       	    <input type="submit" value="Login">
+       	  </div>
+       </div> 
+    </form>
+
+</body>
+</html>
